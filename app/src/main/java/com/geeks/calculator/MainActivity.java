eebbeed282c129c80e2ca45fabc672bf262d325c
megodevice <megodevice14@gmail.com>
2023-11-23 23:28:21 +0600

    only design

---
+++ app/src/main/java/com/geeks/calculator/MainActivity.java
@@ -0,0 +1,24 @@
+package com.geeks.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView resultTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        resultTextView = findViewById(R.id.result_text_view);
+    }
+
+    public void onClick(View view) {
+        resultTextView.setText(((MaterialButton)view).getText().toString());
+    }
+}
